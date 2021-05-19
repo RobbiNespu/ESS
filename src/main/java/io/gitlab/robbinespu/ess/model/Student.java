c32e2f8a1ab87da0a2991c9a013d0836e12a2b83
Robbi Nespu <robbinespu@gmail.com>
2021-05-19 15:59:10 +0800

    unit test for entities name

--- src/main/java/io/gitlab/robbinespu/ess/model/Student.java
+++ src/main/java/io/gitlab/robbinespu/ess/model/Student.java
@@ -6,7 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -18,8 +20,12 @@ public class Student {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
+
     @NotEmpty(message = "name is required")
+    @NotNull(message = "cannot be null")
+    @NotBlank(message = "cannot be blank")
     private String name;
+
     private String department;
     @Column(nullable = true, name = "email")
     private String email;
---
+++ src/test/java/io/gitlab/robbinespu/ess/model/StudentTest.java
@@ -0,0 +1,27 @@
+package io.gitlab.robbinespu.ess.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+
+class StudentTest {
+
+    @Test
+    @DisplayName("Set name - String")
+    public void setNameNotBlank() {
+        Student std = new Student();
+        std.setName("Ayam");
+        assertEquals("Ayam", std.getName());
+    }
+
+    @Test
+    @DisplayName("Set name - null")
+    public void setNameBlank() {
+        Student std = new Student();
+        std.setName(null);
+        assertNotSame(null, std.getName());
+    }
+
+}
