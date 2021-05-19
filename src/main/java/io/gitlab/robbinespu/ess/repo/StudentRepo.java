52d9ff59037ed9024968c9669ebcd576379378c6
Robbi Nespu <robbinespu@gmail.com>
2021-05-19 12:01:41 +0800

    WIP: JpaRepository interface provides you with all the methods you need to manage data: save(), findAll(), count(), delete()…

--- src/main/java/io/gitlab/robbinespu/ess/repo/StudentRepo.java
+++ src/main/java/io/gitlab/robbinespu/ess/repo/StudentRepo.java
@@ -1,7 +1,14 @@
 package io.gitlab.robbinespu.ess.repo;
 
 import io.gitlab.robbinespu.ess.model.Student;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface StudentRepo extends CrudRepository<Student, String> {
+import java.util.Optional;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, String> {
+    // Query method
+    Optional<Student> findByEmail(String email);
+
 }
