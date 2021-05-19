c76412fb26d1212e26e173b1a7a37c3850a52a72
Robbi Nespu <robbinespu@gmail.com>
2021-05-19 11:03:14 +0800

    init project

---
+++ src/test/java/io/gitlab/robbinespu/ess/EssApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitlab.robbinespu.ess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
