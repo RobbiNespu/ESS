c25a94be0d568bd9bb44a2908218dc26a7909148
Robbi Nespu <robbinespu@gmail.com>
2021-05-22 18:20:07 +0800

    Find forms by Id

--- src/main/java/io/robbinespu/ess/repo/FormRepo.java
+++ src/main/java/io/robbinespu/ess/repo/FormRepo.java
@@ -4,6 +4,9 @@ import io.robbinespu.ess.model.Forms;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FormRepo extends JpaRepository<Forms, String> {
+    Optional<Forms> findById(String id);
 }
--- src/main/java/io/robbinespu/ess/service/IFormsService.java
+++ src/main/java/io/robbinespu/ess/service/IFormsService.java
@@ -2,6 +2,10 @@ package io.robbinespu.ess.service;
 
 import io.robbinespu.ess.model.Forms;
 
+import java.util.Optional;
+
 public interface IFormsService {
     Forms save(Forms forms);
+
+    Optional<Forms> findById(String id);
 }
