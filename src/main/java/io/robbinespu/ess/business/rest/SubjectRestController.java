3cb0219e42de8d5027eb633bd1fad2e619eddad2
Robbi Nespu <robbinespu@gmail.com>
2021-05-28 23:55:51 +0800

    Glues the last node - SLOT! (Ugly code applied)

--- src/main/java/io/robbinespu/ess/business/rest/SubjectRestController.java
+++ src/main/java/io/robbinespu/ess/business/rest/SubjectRestController.java
@@ -3,7 +3,7 @@
  *
  * Project :  Advance Software Development - Exam Scheduling System with DFS
  * Class name :  io.robbinespu.ess.business.rest.SubjectRestController
- * Last modified:  5/28/21, 3:17 AM
+ * Last modified:  5/28/21, 11:55 PM
  * User : Robbi Nespu < robbinespu@gmail.com >
  *
  * License : https://github.com/RobbiNespu/ESS/LICENSE
@@ -17,17 +17,18 @@ import io.robbinespu.ess.model.*;
 import io.robbinespu.ess.service.*;
 import io.robbinespu.ess.util.ObjectToJsonObjectNode;
 import io.robbinespu.ess.util.RestControllerHelper;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -159,6 +160,30 @@ public class SubjectRestController extends RestControllerHelper {
     nodeSubjectGroup.setChild("G" + String.valueOf(classSubjectList.getGroupSlot()));
     nodeSubjectGroup.setLevel(4);
     nodeService.save(nodeSubjectGroup);
+
+    // START => UGLIEST code - It maybe blow up! - Slots node edges | handle it with care
+    if (hour == 1) {
+      Nodes slots7 = new Nodes(nodeSubjectGroup.getChild().toString(), "SLOT7", 5); // SL7
+      nodeService.save(slots7);
+    } else if (hour == 2) {
+      Nodes slots1 = new Nodes(nodeSubjectGroup.getChild().toString(), "SLOT1", 5); // SL1
+      nodeService.save(slots1);
+      Nodes slots3 = new Nodes(nodeSubjectGroup.getChild().toString(), "SLOT3", 5); // SL3
+      nodeService.save(slots3);
+      Nodes slots6 = new Nodes(nodeSubjectGroup.getChild().toString(), "SLOT6", 5); // SL6
+      nodeService.save(slots6);
+    } else if (hour == 2) {
+      Nodes slots2 = new Nodes(nodeSubjectGroup.getChild().toString(), "SLOT2", 5); // SL2
+      nodeService.save(slots2);
+      Nodes slots4 = new Nodes(nodeSubjectGroup.getChild().toString(), "SLOT4", 5); // SL4
+      nodeService.save(slots4);
+      Nodes slots5 = new Nodes(nodeSubjectGroup.getChild().toString(), "SLOT5", 5); // SL5
+      nodeService.save(slots5);
+    } else {
+      //heck!
+      throw new CustomRestException("What happen? Pls check");
+    }
+    // END => UGLIEST code - It maybe blow up! - Slots node edges | handle it with care
 
     String subjectsJson = ConvertToJsonString(subjects);
     map = new ObjectMapper().readValue(subjectsJson, HashMap.class);
--- src/main/java/io/robbinespu/ess/model/Nodes.java
+++ src/main/java/io/robbinespu/ess/model/Nodes.java
@@ -3,7 +3,7 @@
  *
  * Project :  Advance Software Development - Exam Scheduling System with DFS
  * Class name :  io.robbinespu.ess.model.Nodes
- * Last modified:  5/28/21, 3:07 AM
+ * Last modified:  5/28/21, 11:55 PM
  * User : Robbi Nespu < robbinespu@gmail.com >
  *
  * License : https://github.com/RobbiNespu/ESS/LICENSE
@@ -12,13 +12,14 @@
 package io.robbinespu.ess.model;
 
 import io.robbinespu.ess.util.CustomSeqGeneratorIdForUser;
-import java.io.Serializable;
-import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Setter
@@ -41,14 +42,20 @@ public class Nodes implements Serializable {
             value = "50"),
         @org.hibernate.annotations.Parameter(
             name = CustomSeqGeneratorIdForUser.VALUE_PREFIX_PARAMETER,
-            value = "node_"),
-        @org.hibernate.annotations.Parameter(
-            name = CustomSeqGeneratorIdForUser.NUMBER_FORMAT_PARAMETER,
-            value = "%03d")
+                value = "node_"),
+              @org.hibernate.annotations.Parameter(
+                      name = CustomSeqGeneratorIdForUser.NUMBER_FORMAT_PARAMETER,
+                      value = "%03d")
       })
   private String id;
 
   private String parent;
   private String child;
   private int level;
+
+  public Nodes(String parent, String child, int level) {
+    this.parent = parent;
+    this.child = child;
+    this.level = level;
+  }
 }
